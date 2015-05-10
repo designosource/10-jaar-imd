59918a1509ca00fc51661b2dd23990a30de04146
AxelVerstappen <axel_verstappen@hotmail.com>
2015-05-10 13:27:14 +0200

    Updated the file save function to store local images

--- routes/index.js
+++ routes/index.js
@@ -25,34 +25,43 @@ router.get('/', function(req, res, next) {
 /* POST to Add Post Service */
 router.post('/addpost', multipartMiddleware, function(req, res) {
     var db = req.db;
+    var collection = db.get('postcollection');
     
-    console.log(req.files);
     var inputUserId = req.user.id;
     var inputName = req.user.displayName;
     var inputMessage = req.body.message;
-    var inputImageType = req.files.image.type;
-    var inputImageData = fs.readFileSync(req.files.image.path);
-    inputImageData = inputImageData.toString('base64');
+    
+    fs.readFile(req.files.image.path, function (err, data) {
 
-    var collection = db.get('postcollection');
+		var imageName = req.files.image.name;
 
-    collection.insert({
-        "user_id" : inputUserId,
-        "name" : inputName,
-        "message" : inputMessage,
-        "img" : {
-            "data": inputImageData,
-            "type": inputImageType
-        }
-    }, function (err, doc) {
-        if (err) {
-            res.send("There was a problem adding the information to the database.");
-        }
-        else {
-            res.location("/");
+		if(!imageName){
             res.redirect("/");
-        }
-    });
+			res.end();
+
+		} else {
+            var newPath = __dirname + "/../public/images/uploads/" + imageName;
+            
+            fs.writeFile(newPath, data, function (err) {
+                collection.insert({
+                    "user_id" : inputUserId,
+                    "name" : inputName,
+                    "message" : inputMessage,
+                    "img" : {
+                        "src": "images/uploads/"+imageName
+                    }
+                }, function (err, doc) {
+                    if (err) {
+                        res.send("There was a problem adding the information to the database.");
+                    }
+                    else {
+                        res.location("/");
+                        res.redirect("/");
+                    }
+                });
+            });
+		}
+	});
 });
 
 /* Facebook login */
