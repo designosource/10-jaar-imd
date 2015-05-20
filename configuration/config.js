46a7bc3d0e99d3eb879fc25fc76bf66d63a4d3a2
AxelVerstappen <axel_verstappen@hotmail.com>
2015-05-20 15:56:47 +0200

    Upload to Heroku

---
+++ configuration/config.js
@@ -0,0 +1,7 @@
+module.exports={
+    "facebook_api_key"      :     "689079691220186",
+    "facebook_api_secret"   :     "0a7fd8b89a090b368c900f8ae7c9b3b4",
+    "callback_url"          :     "http://localhost:3000/auth/facebook/callback",
+    "mongodb_user"          :     "designosource",
+    "mongodb_password"      :     "uniekdigitaalverhaal1"
+}
